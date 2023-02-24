25a2651c041fa4167cf9ca9d38ca7fd273695478
mohamednasser-dev <72683413+mohamednasser-dev@users.noreply.github.com>
2023-02-24 04:35:36 +0200

    appends title in unites

--- app/ProductUnit.php
+++ app/ProductUnit.php
@@ -8,7 +8,20 @@ class ProductUnit extends Model
 {
     protected $guarded = [''];
 
-    public function Producr() {
+    public function Producr()
+    {
         return $this->belongsTo('App\Product', 'product_id');
+    }
+
+    protected $appends = ['title'];
+
+    public function getTitleAttribute()
+    {
+
+        if (\app()->getLocale() == "ar") {
+            return $this->unit_ar;
+        } else {
+            return $this->unit_en;
+        }
     }
 }
